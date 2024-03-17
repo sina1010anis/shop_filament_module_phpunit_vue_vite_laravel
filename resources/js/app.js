24aa851718eda9549243b79ea20d610db30314ad
sina1010anis <sina1010anis@gmail.com>
2024-03-17 08:48:43 +0330

    start

---
+++ resources/js/app.js
@@ -0,0 +1,19 @@
+import './bootstrap';
+
+import {createApp} from 'vue/dist/vue.esm-bundler';
+import HelloWorld from "./components/HelloWorld.vue";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import $ from 'jquery'
+import axios from 'axios';
+
+const app = createApp({
+    data:()=>({
+        infoApp : '(Laravel 10.43) (Vue 3)'
+    }),
+    components:{
+        HelloWorld:HelloWorld
+    }
+})
+
+app.mount('#app')
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        @vite('resources/css/app.css')
+
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div id="app">
+            <p>@{{infoApp}}</p>
+            <hello-world></hello-world>
+        </div>
+    </body>
+
+    @vite('resources/js/app.js')
+</html>
