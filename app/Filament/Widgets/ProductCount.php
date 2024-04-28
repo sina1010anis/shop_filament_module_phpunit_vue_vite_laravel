c1072575505d131a16d66acf076830bf32e5deea
sina1010anis <sina1010anis@gmail.com>
2024-04-28 09:15:46 +0330

    widget new

--- app/Filament/Widgets/ProductCount.php
+++ app/Filament/Widgets/ProductCount.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Modules\Front\App\Models\Product;
@@ -11,7 +12,9 @@ class ProductCount extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total Product', Product::count())->description('Total of number Menu Product')->descriptionIcon('heroicon-o-archive-box')->color('success')->chart([5, 8, 1, 2, 5, 6])
+            Stat::make('Total Product', Product::count())->description('Total of number Menu Product')->descriptionIcon('heroicon-o-archive-box')->color('success')->chart([5, 8, 1, 2, 5, 6]),
+            Stat::make('Total User', User::count())->description('Total of number Menu User')->descriptionIcon('heroicon-o-archive-box')->color('danger')->chart([8, 5, 3, 4, 8, 1]),
+            Stat::make('Total View', Product::sum('view'))->description('Total of number Menu View')->descriptionIcon('heroicon-o-archive-box')->color('info')->chart([5, 8, 1, 2, 8, 9])
 
         ];
     }
--- app/Filament/Widgets/Status0Overflow.php
+++ app/Filament/Widgets/Status0Overflow.php
@@ -2,29 +2,72 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Filament\Widgets\ChartWidget;
-use Filament\Widgets\StatsOverviewWidget\Stat;
 use Modules\Front\App\Models\Product;
+use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class Status0Overflow extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Number Of User';
+
+    protected int | string | array $columnSpan = 'full';
+
+    private $number_of_user;
+    private $number_mothe;
 
     protected function getData(): array
     {
+        $data = $this->getNumberOfUser();
+
+        $mothe = $this->getMothe();
+
         return [
             'datasets' => [
                 [
-                    'label' => 'Prodict Price',
-                    'data' => [5, 9, 7, 2, 1, 5, 6, 8]
+                    'label' => 'number of user',
+                    'data' => $this->number_of_user
                 ]
             ],
-            'labels' => ['W1', 'W2', 'W3', 'WW1', 'WW2', 'WW3', 'RE1', 'RE2']
+            'labels' => $this->number_mothe
         ];
     }
 
     protected function getType(): string
     {
-        return 'pie';
+        return 'line';
+    }
+
+    private function getNumberOfUser(): void
+    {
+        $year = Carbon::now()->year;
+
+        $new_orders_count = [];
+
+        for($i = 1; $i <= 12; $i++){
+
+            $new_orders_count[] = User::whereYear('updated_at', $year)->whereMonth('updated_at', $i)->count();
+
+        }
+
+        $this->number_of_user = $new_orders_count;
+
+    }
+
+    private function getMothe(): void
+    {
+        $month = [];
+
+
+        for ($m=1; $m<=12; $m++) {
+
+            $month[] = date('F', mktime(0,0,0,$m, 1, date('Y')));
+
+        }
+
+        $this->number_mothe = $month;
+
+
     }
 }
